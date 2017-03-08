4b9982202cb1239933733bfc3178d98aac96e5d6
Juha Huuhka <jhuuhka@gmail.com>
2017-03-08 22:00:26 +0200

    Proto PlatoformIO + Clion project

---
+++ clion/proto/cmake-build-debug/CMakeFiles/wifi.dir/DependInfo.cmake
@@ -0,0 +1,64 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/jhuuhka/Arduino/git/clion/wifi/src/init.cpp" "/home/jhuuhka/Arduino/git/clion/wifi/cmake-build-debug/CMakeFiles/wifi.dir/src/init.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "ARDUINO=20300"
+  "ARDUINO_ARCH_ESP8266"
+  "ESP8266"
+  "ESP8266_WEMOS_D1MINI"
+  "F_CPU=80000000L"
+  "ICACHE_FLASH"
+  "LWIP_OPEN_SRC"
+  "PLATFORMIO=30201"
+  "__ets__"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/home/jhuuhka/.platformio/packages/framework-arduinoespressif8266/tools/sdk/include"
+  "/home/jhuuhka/.platformio/packages/framework-arduinoespressif8266/tools/sdk/lwip/include"
+  "/home/jhuuhka/.platformio/packages/framework-arduinoespressif8266/cores/esp8266"
+  "/home/jhuuhka/.platformio/packages/framework-arduinoespressif8266/variants/d1_mini"
+  "../src"
+  "/home/jhuuhka/.platformio/packages/framework-arduinoespressif8266/libraries/ArduinoOTA"
+  "/home/jhuuhka/.platformio/packages/framework-arduinoespressif8266/libraries/DNSServer/src"
+  "/home/jhuuhka/.platformio/packages/framework-arduinoespressif8266/libraries/EEPROM"
+  "/home/jhuuhka/.platformio/packages/framework-arduinoespressif8266/libraries/ESP8266AVRISP/src"
+  "/home/jhuuhka/.platformio/packages/framework-arduinoespressif8266/libraries/ESP8266HTTPClient/src"
+  "/home/jhuuhka/.platformio/packages/framework-arduinoespressif8266/libraries/ESP8266HTTPUpdateServer/src"
+  "/home/jhuuhka/.platformio/packages/framework-arduinoespressif8266/libraries/ESP8266SSDP"
+  "/home/jhuuhka/.platformio/packages/framework-arduinoespressif8266/libraries/ESP8266WebServer/src"
+  "/home/jhuuhka/.platformio/packages/framework-arduinoespressif8266/libraries/ESP8266WiFi/src"
+  "/home/jhuuhka/.platformio/packages/framework-arduinoespressif8266/libraries/ESP8266WiFiMesh/src"
+  "/home/jhuuhka/.platformio/packages/framework-arduinoespressif8266/libraries/ESP8266httpUpdate/src"
+  "/home/jhuuhka/.platformio/packages/framework-arduinoespressif8266/libraries/ESP8266mDNS"
+  "/home/jhuuhka/.platformio/packages/framework-arduinoespressif8266/libraries/Ethernet/src"
+  "/home/jhuuhka/.platformio/packages/framework-arduinoespressif8266/libraries/GDBStub/src"
+  "/home/jhuuhka/.platformio/packages/framework-arduinoespressif8266/libraries/Hash/src"
+  "/home/jhuuhka/.platformio/packages/framework-arduinoespressif8266/libraries/SD/src"
+  "/home/jhuuhka/.platformio/packages/framework-arduinoespressif8266/libraries/SPI"
+  "/home/jhuuhka/.platformio/packages/framework-arduinoespressif8266/libraries/Servo/src"
+  "/home/jhuuhka/.platformio/packages/framework-arduinoespressif8266/libraries/SoftwareSerial"
+  "/home/jhuuhka/.platformio/packages/framework-arduinoespressif8266/libraries/TFT_Touch_Shield_V2"
+  "/home/jhuuhka/.platformio/packages/framework-arduinoespressif8266/libraries/Ticker"
+  "/home/jhuuhka/.platformio/packages/framework-arduinoespressif8266/libraries/Wire"
+  "/home/jhuuhka/.platformio/packages/framework-arduinoespressif8266/libraries/esp8266/src"
+  "/home/jhuuhka/.platformio/packages/toolchain-xtensa/xtensa-lx106-elf/include"
+  "/home/jhuuhka/.platformio/packages/toolchain-xtensa/lib/gcc/xtensa-lx106-elf/4.8.2/include-fixed"
+  "/home/jhuuhka/.platformio/packages/toolchain-xtensa/lib/gcc/xtensa-lx106-elf/4.8.2/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
