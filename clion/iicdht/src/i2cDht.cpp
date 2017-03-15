f1f27c03abde069a0b99fa9578350133d23de81a
Juha Huuhka <jhuuhka@gmail.com>
2017-03-15 23:32:19 +0200

    Fonts

--- clion/iicdht/src/i2cDht.cpp
+++ clion/iicdht/src/i2cDht.cpp
@@ -1,6 +1,8 @@
 #include <Arduino.h>
 #include <Adafruit_ssd1306syp.h>
 #include <DHT.h>
+#include <Fonts/FreeSans24pt7b.h>
+#include <Fonts/FreeSans12pt7b.h>
 
 #define DHTTYPE DHT22
 #define DHTPIN 10
@@ -15,23 +17,27 @@ void setup() {
     dht.readTemperature();
 
     display.initialize();
-    display.setTextSize(5);
-    display.print(" ...");
+    display.setTextSize(1);
+    display.setFont(&FreeSans12pt7b);
+    display.setCursor(0,40);
+    display.print("Hello world!");
     display.update();
     delay(3000);
+    display.setFont(&FreeSans24pt7b);
 
 }
 void loop() {
     float t = dht.readTemperature();
     display.clear();
-    display.setCursor(0, 0);
+    display.setCursor(0,50);
 
     display.print(t, 1);
+    display.print("C");
     display.update();
     delay(3000);
     display.clear();
-    display.setCursor(10,2);
-    display.print("...");
+    display.setCursor(0,50);
+    display.print("   ...");
     display.update();
     delay(300);
     display.update();
